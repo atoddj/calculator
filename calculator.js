1c958dcd2623c7adc12210d1db6141c3d61ca21f
Todd Jones <atoddj@gmail.com>
2015-12-20 01:28:39 -0500

    Made simple calculator functions on click

--- calculator.js
+++ calculator.js
@@ -1,1 +1,16 @@
 //calculator.js
+var calculation;
+$("a").click(function(){
+	var value = $(this).html();
+	if (value=="=") {
+		//perform math when equal sign is clicked
+		calculation = Number(eval($("#calcInput").val())); // learned about eval function
+		$("#calcInput").val(calculation);
+
+	} else if(value=="CLR") {
+		$("#calcInput").val('');
+	} else {
+		$("#calcInput").val($('#calcInput').val() + value); // sets calculator display
+	}
+	
+});
